8a04ede7f6b8602882076df742803f2a94409c82
taras <zahreba@evergreen.in.ua>
2021-05-22 11:44:48 +0300

    spaces

--- banner.php
+++ banner.php
@@ -8,21 +8,21 @@ $agent = $_SERVER["HTTP_USER_AGENT"];
 $url = $_SERVER['HTTP_REFERER'];
 
 $stmt = $dbh->prepare("SELECT views_count FROM log WHERE ip_address=:ip AND user_agent=:agent AND page_url=:url");
-$stmt->bindParam(':ip',$ip);
-$stmt->bindParam(':agent',$agent);
-$stmt->bindParam(':url',$url);
+$stmt->bindParam(':ip', $ip);
+$stmt->bindParam(':agent', $agent);
+$stmt->bindParam(':url', $url);
 $stmt->execute();
 $count = $stmt->rowCount();
 
-$date = date ("Y-m-d H:i:s",time());
-if ($count==0) {
+$date = date("Y-m-d H:i:s", time());
+if ($count == 0) {
     $stmt = $dbh->prepare("INSERT INTO log (ip_address, user_agent, page_url, view_date, views_count) VALUES(:ip,:agent,:url,:view_date,:v_count)");
     $count++;
-    $stmt->bindParam(':ip',$ip);
-    $stmt->bindParam(':agent',$agent);
-    $stmt->bindParam(':url',$url);
-    $stmt->bindParam(':view_date',$date);
-    $stmt->bindParam(':v_count',$count);
+    $stmt->bindParam(':ip', $ip);
+    $stmt->bindParam(':agent', $agent);
+    $stmt->bindParam(':url', $url);
+    $stmt->bindParam(':view_date', $date);
+    $stmt->bindParam(':v_count', $count);
     $stmt->execute();
 } else {
     $row = $stmt->fetch();
@@ -30,11 +30,11 @@ if ($count==0) {
     $viewsCount++;
     $stmt = $dbh->prepare("UPDATE log SET view_date=:view_date,views_count=:v_count 
         WHERE ip_address=:ip AND user_agent=:agent AND page_url=:url");
-    $stmt->bindParam(':ip',$ip);
-    $stmt->bindParam(':agent',$agent);
-    $stmt->bindParam(':url',$url);
-    $stmt->bindParam(':view_date',$date);
-    $stmt->bindParam(':v_count',$viewsCount);
+    $stmt->bindParam(':ip', $ip);
+    $stmt->bindParam(':agent', $agent);
+    $stmt->bindParam(':url', $url);
+    $stmt->bindParam(':view_date', $date);
+    $stmt->bindParam(':v_count', $viewsCount);
     $stmt->execute();
 }
 $content = file_get_contents("logo.svg");
@@ -44,7 +44,8 @@ header("Content-Length: " . strlen($content));
 
 echo $content;
 
-function getIp() {
+function getIp()
+{
     $keys = [
         'HTTP_CLIENT_IP',
         'HTTP_X_FORWARDED_FOR',
